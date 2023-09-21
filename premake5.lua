47e90bfbf3c71b084d49a31cf4de962ef1c48c3b
bidus92 <bidusblogs@gmail.com>
2023-09-21 10:50:42 -0400

    updating Dear_ImGui with premake file for submodule use

---
+++ premake5.lua
@@ -0,0 +1,37 @@
+project "Dear_ImGui"
+    kind "StaticLib"
+    language "C++"
+    staticruntime "On"
+	
+	
+    targetdir("bin/" .. outputdir .. "/%{prj.name}")
+    objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+
+    files
+    {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_tables.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp"
+    }
+
+
+    filter "configuration:Debug"
+        systemversion "latest"
+        buildoptions "/MTd"
+		defines "_DEBUG"
+		symbols "On"
+
+	filter "configuration:Release"
+	    systemversion "latest"
+		buildoptions "/MT"
+        defines "NDEBUG"
+		optimize "On"
